75213fab94f28a8b5f44c5a996e50c3693ff352e
Parth Goel <parthgoel1204@gmail.com>
2026-04-03 23:24:22 +0530

    feat: added logo and background image

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = ()=> {
+    return(
+        <div className="absolute px-8 py-6 bg-linear-to-b from-black">
+            <img 
+                src="/netflix.svg" 
+                alt="Netflix Logo" 
+                className="w-40"
+            />
+        </div>
+    );
+};
+
+export default Header;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,16 @@
+import Header from "./Header";
 const Login = () => {
     return (
-    <>
-    Login
-    </>
+    <div>
+        <Header/>
+        <div>
+            <img 
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/b9448d14-5983-4ffc-a4d6-e22223108466/web/IN-en-20260302-TRIFECTA-perspective_1ef91182-c674-4632-9bec-d185993ab154_large.jpg"
+            alt="backgroundImg"
+            />
+        </div>
+
+    </div>
     )
 };
 
