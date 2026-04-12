a5d497ee77b29ded5a2b29b3174aae1d7e935f9c
Parth Goel <parthgoel1204@gmail.com>
2026-04-12 19:41:30 +0530

    feat: added components for Browse page

---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
+
+const MainContainer = () => {
+    const movies = useSelector(store => store.movies?.nowPlayingMovies);
+    if(!movies) return;
+    const mainMovie = movies[0];
+    return (
+        <div>
+            <VideoTitle/>;
+            <VideoBackground/>; 
+        </div>
+    )
+}
+export default MainContainer;
