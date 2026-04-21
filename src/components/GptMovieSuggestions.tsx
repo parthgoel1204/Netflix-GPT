8f6c2eea66202c0443082b1e2ab17f9707b150a2
Parth Goel <parthgoel1204@gmail.com>
2026-04-21 21:50:06 +0530

    feat: displayed the movieNames along with their titles on the gpt page

--- src/components/GptMovieSuggestions.tsx
+++ src/components/GptMovieSuggestions.tsx
@@ -1,7 +1,28 @@
+import { useAppSelector } from "../utils/hooks";
+import MovieList from "./MovieList";
+
+interface Movie {
+    id: number;
+    poster_path: string;
+}
 const GptMovieSuggestions = () => {
+    const {movieNames , movieResults} = useAppSelector(store => store.gpt) as {
+         movieNames: string[] | null;
+        movieResults: Movie[][] | null;
+    };
+     if (!movieNames || !movieResults) return null;
+
     return(
-        <div>
-            ns
+        <div className="p-4 m-4 bg-black/70 text-white ">
+            <div>
+                {movieNames.map((movieName: string, index: number) => (
+                    <MovieList
+                        key={movieName}
+                        title={movieName}
+                        movies={movieResults[index]}
+                    />
+                ))}
+            </div>
         </div>
     )
 };
